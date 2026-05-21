ab9b8f7b6605236aed8d400aa40f25d1f65b1a54
Yagocke <yagocko0@gmail.com>
2026-05-20 22:42:56 -0600

    Quitar los cert

--- hotel_backend/settings.py
+++ hotel_backend/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^9!k1h!g8jjh_de)jbaduhp_#jzs0vj6_x93r4@*i4s*$si%)q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
